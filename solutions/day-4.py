8348007596e31d75e1d485495cce72ab78ccc3f3
Jacty <saej3101@usherbrooke.ca>
2022-12-04 14:55:41 -0500

    Added day 4 solution part two

--- solutions/day-4.py
+++ solutions/day-4.py
@@ -5,15 +5,22 @@ def is_between(p1, p2):
     return (p1[0] <= p2[0] and p1[1] >= p2[1]) or (p2[0] <= p1[0] and p2[1] >= p1[1])
 
 
+def overlaps(p1, p2):
+    return (p1[0] <= p2[0] <= p1[1] or p1[0] <= p2[1] <= p1[1]) or (p2[0] <= p1[0] <= p2[1] or p2[0] <= p1[1] <= p2[1])
+
+
 pairs = read_file('inputs/day-4.txt')
 
-fc = 0
+fc1 = 0
+fc2 = 0
 for pair in pairs:
     sp = pair.strip().split(',')
 
     first = list(map(int, sp[0].split('-')))
     second = list(map(int, sp[1].split('-')))
 
-    fc += 1 if is_between(first, second) else 0
+    fc1 += 1 if is_between(first, second) else 0
+    fc2 += 1 if overlaps(first, second) else 0
 
-print(fc)
+print(fc1)
+print(fc2)
