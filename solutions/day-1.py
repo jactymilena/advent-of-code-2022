9df70ac8ce20a11262e5866c597abee5139fdeaf
Jacty <saej3101@usherbrooke.ca>
2022-12-04 10:40:47 -0500

    Added day 1 - part 2

--- solutions/day-1.py
+++ solutions/day-1.py
@@ -3,14 +3,16 @@ from utils.reader import read_file
 calories = read_file('inputs/day-1.txt')
 
 calories_sum = 0
-max_calories = 0
+max_calories = []
 
 for calorie in calories:
     if calorie != '\n':
         calories_sum += int(calorie)
     else:
-        max_calories = max(calories_sum, max_calories)
+        max_calories.append(calories_sum)
         calories_sum = 0
 
-print(f'max calories : {max_calories}')
+max_calories.sort(reverse=True)
+print(f'max calories : {max_calories[0:3]}')
+print(f'total max calories : {sum(max_calories[0:3])}')
 
